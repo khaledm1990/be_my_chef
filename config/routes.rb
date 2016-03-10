d290b27a4e92fc831521faa7ebad759493ea4616
Seth <Sethlee99@gmail.com>
2016-03-10 18:23:47 +0800

    style index page 2

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
       }
 
 
-  root "events#index"
+  root "static#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
