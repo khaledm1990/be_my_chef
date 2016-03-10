a12ce493fffa6973fd30c9c353cc21d37c0944db
khaled muamar <khaled@khaleds-MacBook-Pro.local>
2016-03-10 15:25:57 +0800

    redirect user to show page after update

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -22,9 +22,28 @@ before_filter :configure_account_update_params, only: [:update]
   end
 
   # PUT /resource
-  # def update
-  #   super
-  # end
+ def update
+  byebug
+    self.resource = resource_class.to_adapter.get!(send(:"current_#{resource_name}").to_key)
+    prev_unconfirmed_email = resource.unconfirmed_email if resource.respond_to?(:unconfirmed_email)
+
+    current_user.remove_image! if params[:user][:remove_image] == "1"
+    byebug
+    resource_updated = update_resource(resource, account_update_params)
+    yield resource if block_given?
+    if resource_updated
+      if is_flashing_format?
+        flash_key = update_needs_confirmation?(resource, prev_unconfirmed_email) ?
+          :update_needs_confirmation : :updated
+        set_flash_message :notice, flash_key
+      end
+      sign_in resource_name, resource, bypass: true
+      respond_with resource, location: after_update_path_for(resource)
+    else
+      clean_up_passwords resource
+      respond_with resource
+    end
+  end
 
   # DELETE /resource
   # def destroy
