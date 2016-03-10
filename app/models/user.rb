0d82c8a7b8abffc6b18815b61f8d4390934989e5
tongwei <jeanwei938@msn.com>
2016-03-10 17:34:42 +0800

    update events model,controller,view

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ActiveRecord::Base
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  has_many :events, dependent: :destroy
  # before_action :authenticate_user!
 end
