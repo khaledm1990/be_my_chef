2c81ad27a4d49241f67accc63ef5a8e6d81def7f
khaled muamar <khaled@khaleds-MacBook-Pro.local>
2016-03-11 09:15:08 +0800

    event migration table changed

--- db/migrate/20160309013726_create_events.rb
+++ db/migrate/20160309013726_create_events.rb
@@ -2,11 +2,15 @@ class CreateEvents < ActiveRecord::Migration
   def change
     create_table :events do |t|
       t.integer :user_id
-      t.string :title
-      t.string :description
+      t.string :name
       t.date :date
-      t.integer :guest_number
+      t.time :starting_time
+      t.time :ending_time
+      t.string :pax
+      t.string :variety
+      t.string :description
       t.timestamps null: false
+
     end
   end
 end
