a6029aa21d14a389c302385f4d100ee2ebade0c6
Seth <Sethlee99@gmail.com>
2016-03-11 12:18:42 +0800

    fill

--- db/migrate/20160309013726_create_events.rb
+++ db/migrate/20160309013726_create_events.rb
@@ -2,10 +2,6 @@ class CreateEvents < ActiveRecord::Migration
   def change
     create_table :events do |t|
       t.integer :user_id
-      t.string :title
-      t.string :description
-      t.date :date
-      t.integer :guest_number
       t.timestamps null: false
     end
   end
